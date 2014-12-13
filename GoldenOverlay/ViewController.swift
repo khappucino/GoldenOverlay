36f8a6716be2c6beb821783561383b6a14a25d50
khappucino <david.capino@gmail.com>
2014-12-13 15:34:15 -0800

    Initial Commit

---
+++ GoldenOverlay/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoldenOverlay
+//
+//  Created by spacehomunculus on 12/13/14.
+//  Copyright (c) 2014 electricbaconstudios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
